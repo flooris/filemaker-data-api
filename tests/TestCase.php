5a403d88c3fa51523d600c9133046de01e3ec4e4
Tim <t.blommestijn@flooris.nl>
2023-12-12 17:05:38 +0100

    formatted test case

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,9 @@
 
 namespace Flooris\FileMakerDataApi\Tests;
 
-use Flooris\FileMakerDataApi\FileMakerDataApiServiceProvider;
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Orchestra\Testbench\TestCase as Orchestra;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Flooris\FileMakerDataApi\FileMakerDataApiServiceProvider;
 
 class TestCase extends Orchestra
 {
@@ -13,7 +13,7 @@ class TestCase extends Orchestra
         parent::setUp();
 
         Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'FileMakerDataApi\\FileMakerDataApi\\Database\\Factories\\'.class_basename($modelName).'Factory'
+            fn (string $modelName) => 'FileMakerDataApi\\FileMakerDataApi\\Database\\Factories\\' . class_basename($modelName) . 'Factory'
         );
     }
 
